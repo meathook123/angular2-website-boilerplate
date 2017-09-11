bff83e30e5024463c13e1416779242dfbd9ee153
Ruiyang Sun <sunruiyang09@gmail.com>
2017-09-11 14:53:00 -0700

    remove unused package

--- src/app/marketing/views/competition/competition.component.ts
+++ src/app/marketing/views/competition/competition.component.ts
@@ -1,5 +1,4 @@
-import { Component, OnInit, ViewChild } from '@angular/core';
-import { CalendarComponent } from "ap-angular2-fullcalendar";
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-marketing-competition',
@@ -7,78 +6,8 @@ import { CalendarComponent } from "ap-angular2-fullcalendar";
   styleUrls: ['./competition.component.scss']
 })
 export class CompetitionComponent implements OnInit {
-  @ViewChild(CalendarComponent) myCalendar: CalendarComponent;
-  private calendarOptions:Object = {
-    // height: 'parent',
-    fixedWeekCount : false,
-    editable: false,
-    eventLimit: true, // allow "more" link when too many events
-    events: [
-      {
-        title: 'All Day Event',
-        start: '2017-09-01',
-        description: 'this is a description'
-      },
-      {
-        title: 'Long Event',
-        start: '2017-09-07',
-        end: '2017-09-10'
-      },
-      {
-        id: 999,
-        title: 'Repeating Event',
-        start: '2017-09-09T16:00:00'
-      },
-      {
-        id: 999,
-        title: 'Repeating Event',
-        start: '2017-09-16T16:00:00'
-      },
-      {
-        title: 'Conference',
-        start: '2017-09-11',
-        end: '2017-09-13'
-      },
-      {
-        title: 'Meeting',
-        start: '2017-09-12T10:30:00',
-        end: '2017-09-12T12:30:00'
-      },
-      {
-        title: 'Lunch',
-        start: '2017-09-12T12:00:00'
-      },
-      {
-        title: 'Meeting',
-        start: '2017-09-12T14:30:00'
-      },
-      {
-        title: 'Happy Hour',
-        start: '2017-09-12T17:30:00'
-      },
-      {
-        title: 'Dinner',
-        start: '2017-09-12T20:00:00'
-      },
-      {
-        title: 'Birthday Party',
-        start: '2017-09-13T07:00:00'
-      }
-    ],
-    eventClick: function(calEvent, jsEvent, view) {
-      console.log(calEvent);
-      console.log(jsEvent);
-      console.log(view);
-      let target = jsEvent.currentTarget;
-      target.insertAdjacentHTML('beforebegin', '<div class="popover bs-popover-top"><h3></h3>two</div>');
-    }
-  };
   constructor() { }
 
   ngOnInit() {
-  }
-
-  onCalendarInit($event) {
-    console.log($event)
   }
 }
